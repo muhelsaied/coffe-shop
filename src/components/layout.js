3dec0a3f488f72a2ab7ba25aed024ab39e74df99
Muhamed Elsaied <muhahmed2099@gamil.com>
2019-04-21 17:47:47 +0200

    navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+
 // import bootstrap
 import BootStrap from "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+// import navbar
+
+import NavBar from "./skeleton/navbar"
+const Layout = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ src/components/skeleton/navbar.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+
+import logo from "../../images/logo.png"
+
+import { FaShoppingCart } from "react-icons/fa"
+export default class NavBar extends Component {
+  state = {
+    navBarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+  navBarHandle = () => {
+    this.state.navBarOpen
+      ? this.setState({
+          navBarOpen: false,
+          css: "collapse navbar-collapse ",
+        })
+      : this.setState({
+          navBarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md nav-bar navbar-dark "
+        style={{background:"#1b2127"}}>
+        <Link to="/" className="">
+          {/* https://www.iconfinder.com/icons/71188/coffee_cup_mug_icon */}
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "60px", margin: "20px" }}
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          onClick={this.navBarHandle}
+          style={{ outlineColor: "var(--mainColor)" }}
+          type="button"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li className="nav-item" key={link.id}>
+                  <Link className="nav-link text-uppercase" to={link.path}>
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+                <li className="text-uppercase text-danger nav-item mt-2 mb-2">
+                    <FaShoppingCart className='cart-icon'/>
+                </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
