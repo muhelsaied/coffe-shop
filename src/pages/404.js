e34bb025b3c2cbac5271891f2516df203560d590
Muhamed Elsaied <muhahmed2099@gamil.com>
2019-04-22 16:56:06 +0200

    notfoundpage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,32 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackGround from "../components/skeleton/backGround"
-import Info from "../components/skeleton/homePage/info"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <BackGround
+      img={data.img.childImageSharp.fluid}
+      title="404 not found"
+      styleClass="not-found-bg"
+    >
+      <h3 className="text-uppercase text-center text-danger my-3 text-weight-bold">
+        could not found your page
+      </h3>
+      <Link
+        to="/"
+        className="text-uppercase text-center btn btn-light btn-outline-danger mx-auto m-2 px-3"
+      >
+        back to home page
+      </Link>
+    </BackGround>
   </Layout>
 )
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "bg.jpg" }) {
+    img: file(relativePath: { eq: "bg-not-found.jpg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
