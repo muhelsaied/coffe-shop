77fcf62eb9e6b156200ba2c75b2ec1fb74569e25
muhelsaied <42692713+muhelsaied@users.noreply.github.com>
2019-04-30 20:12:02 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = ({ data }) => (
       title="Hot coffe Make's your day"
       styleClass="homepage-background"
     />
-    <Info />
+    <Info path='/about'/>
     <Menu items={data.menu} />
     <Products />
     <Contact />
